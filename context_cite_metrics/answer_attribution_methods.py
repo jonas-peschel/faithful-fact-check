dd743d96d5b7186f2b5d13e357c0c452ecb9e0c1
jonas-peschel <jonaspeschel@gmx.de>
2026-02-15 17:18:33 +0100

    reduce default value for ContextCiter batch_size

--- context_cite_metrics/answer_attribution_methods.py
+++ context_cite_metrics/answer_attribution_methods.py
@@ -31,7 +31,7 @@ def parse_args():
     parser.add_argument("--cc_num_ablations", type=int, nargs="+", choices=[32, 64, 128, 256], help="How many ablations to use if ContextCite is used as attribution method.")
     parser.add_argument("--results_path", type=str, default="Results/results.json", help="Path to the file where attribution scores and experiment results are stored.")
     parser.add_argument("--n_samples", type=int, default=20, help="How many data points to sample from the dataset.")
-    parser.add_argument("--cc_batch_size", type=int, default=16, help="Batch size to use in ContextCiter for performing inference using ablated contexts.")
+    parser.add_argument("--cc_batch_size", type=int, default=8, help="Batch size to use in ContextCiter for performing inference using ablated contexts.")
 
     return parser.parse_args()
 
--- context_cite_metrics/calc_cc_metrics.py
+++ context_cite_metrics/calc_cc_metrics.py
@@ -29,7 +29,7 @@ def parse_args():
     parser.add_argument("--results_path", type=str, default="Results/results.json", help="Path to the file where attribution scores and experiment results (metrics) are stored.")
     parser.add_argument("--n_samples", type=int, default=20, help="For how many data points to compute the metrics.")
     parser.add_argument("--m", type=int, default=128, help="How many random ablation vectors to sample for LDS calculation.")
-    parser.add_argument("--cc_batch_size", type=int, default=16, help="Batch size to use in ContextCiter for performing inference using ablated contexts.")
+    parser.add_argument("--cc_batch_size", type=int, default=8, help="Batch size to use in ContextCiter for performing inference using ablated contexts.")
 
     return parser.parse_args()
 
