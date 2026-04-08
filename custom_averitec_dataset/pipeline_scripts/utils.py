b4b5dfdfd59cc21f7b365015cb7385f51feb2599
jonas-peschel <jonaspeschel@gmx.de>
2026-04-08 20:16:03 +0200

    generalize BM25 implementation to work for multiple queries using max pooling; return scores only

--- custom_averitec_dataset/pipeline_scripts/utils.py
+++ custom_averitec_dataset/pipeline_scripts/utils.py
@@ -1,4 +1,8 @@
 import json
+import numpy as np 
+from rank_bm25 import BM25Okapi 
+from nltk import word_tokenize 
+from typing import List 
 
 # json loading and saving
 def load_json(filepath):
@@ -12,3 +16,24 @@ def save_json(filepath, content):
 
     with open(filepath, "w") as f:
         json.dump(content, f, indent=4)
+
+# BM25 for evidence ranking
+class BM25:
+    def __init__(self, corpus: List[str]):
+        self.corpus = corpus 
+        tok_corpus = [word_tokenize(paragraph) for paragraph in corpus]
+        self.retriever = BM25Okapi(tok_corpus)
+    
+    def get_scores(self, query: str | List[str]):
+        if isinstance(query, str):
+            tok_query = word_tokenize(query) 
+            scores = self.retriever.get_scores(tok_query)
+        elif isinstance(query, List):
+            scores = []
+            for q in query:
+                tok_query = word_tokenize(q)
+                scores.append(self.retriever.get_scores(tok_query))
+            scores = np.array(scores)
+            scores = np.max(scores, axis=0)
+
+        return scores
